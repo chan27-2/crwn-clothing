8165c1235bd893c342feaf12da5a0e0aca130c66
Sai Chandan Kadarla <chandankadarla27220022gmail.com>
2021-03-24 22:16:19 +0530

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import "./homepage.styles.scss";
+
+const HomePage = (props) => (
+  <div className="homepage">
+    <div className="menu-holder">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>{" "}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>{" "}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>{" "}
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Title</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
