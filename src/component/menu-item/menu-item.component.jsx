50162d8f5ce5f4d907db7e551f9cc7c11fe62231
Sai Chandan Kadarla <chandankadarla27220022gmail.com>
2021-03-24 23:43:50 +0530

    added homepage Menu Holder, with menuItems and added a bit of styling to it

---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import "../menu-item/menu-item.styles.scss";
+
+const MenuItem = ({ title, size, imageUrl }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    ></div>
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import MenuHolder from "../component/menu-holder/menu-holder.component";
+
+import "./homepage.styles.scss";
+
+const HomePage = (props) => (
+  <div className="homepage">
+    <MenuHolder />
+  </div>
+);
+
+export default HomePage;
